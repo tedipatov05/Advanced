a8b1db1a5925d2c42bfcb342ea3a485144cd19b5
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-04 11:25:59 +0200

    Iterators and Comparators

---
+++ Iterators and Comparators/01. Library/Library.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    internal class Library : IEnumerable<Book>
+    {
+        
+
+        private List<Book> Books;
+
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+
+            private int position = -1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+                Reset();
+            }
+            public Book Current => this.books[position];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+                
+            }
+
+            public bool MoveNext()
+            {
+               this.position++;
+                return position < books.Count;
+            }
+
+            public void Reset()
+            {
+                this.position = -1;
+            }
+        }
+    }
+}
---
+++ Iterators and Comparators/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> ints = { 3, 5, 7,, 9 };
+
+           
+            
+        }
+    }
+}
