16c5ea0124fa1f7083f0cae1941e6e0aa827a409
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-10 09:19:01 +0200

    Exam Prep 6

---
+++ Exam Preparation 6/01. Meal Plan/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace _01._Meal_Plan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> mealsCalories = new Dictionary<string, int>
+            {
+                {"salad", 350},
+                {"soup", 490},
+                {"pasta", 680},
+                {"steak", 790}
+            };
+
+            Queue<string> keysQueue = new Queue<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            Stack<int> valuesStack = new Stack<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int startMealsCount = keysQueue.Count;
+
+            while ((keysQueue.Count > 0) && (valuesStack.Count > 0))
+            {
+                int currentCallories = valuesStack.Pop();
+
+                int currentMealCallories = mealsCalories[keysQueue.Peek()];
+
+                if (currentCallories - currentMealCallories >= 0)
+                {
+                    currentCallories -= currentMealCallories;
+                    keysQueue.Dequeue();
+                    valuesStack.Push(currentCallories);
+                }
+                else if (valuesStack.Count > 0)
+                {
+                    currentCallories -= currentMealCallories;
+                    currentCallories += valuesStack.Pop();
+                    if (currentCallories > 0)
+                    {
+                        valuesStack.Push(currentCallories);
+                        keysQueue.Dequeue();
+                    }
+
+                    if (valuesStack.Count == 0)
+                    {
+                        keysQueue.Dequeue();
+                    }
+                }
+                else
+                {
+                    if (valuesStack.Count == 0)
+                    {
+                        keysQueue.Dequeue();
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            if (valuesStack.Count > 0)
+            {
+                sb.AppendLine($"John had {startMealsCount} meals.");
+                sb.AppendLine($"For the next few days, he can eat {string.Join(", ", valuesStack)} calories.");
+            }
+            else
+            {
+                sb.AppendLine($"John ate enough, he had {startMealsCount - keysQueue.Count} meals.");
+                sb.AppendLine($"Meals left: {string.Join(", ", keysQueue)}.");
+
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+
+        }
+    }
+}
---
+++ Exam Preparation 6/02. Truffle Hunter/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace _02._Truffle_Hunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+
+            int countOfBlack = 0;
+            int countOfSummer = 0;
+            int countOfWhite = 0;
+            int wildBoarEaten = 0;
+
+            char[,] forest = new char[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] rowChars = Console.ReadLine().Split(' ').ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    forest[i, j] = rowChars[j][0];
+                }
+            }
+
+            string command = Console.ReadLine();
+            while(command != "Stop the hunt")
+            {
+                string[] cmdArgs = command.Split(' ');
+                string type = cmdArgs[0];
+                int row = int.Parse(cmdArgs[1]);    
+                int col = int.Parse(cmdArgs[2]);
+                if (IsInForest(row, col, forest))
+                { 
+                    if (type == "Collect")
+                    {
+                        if (forest[row, col] == 'B')
+                        {
+                            countOfBlack++;
+                            forest[row, col] = '-';
+                        }
+                        else if(forest[row, col] == 'S')
+                        {
+                            countOfSummer++;
+                            forest[row, col] = '-';
+                        }
+                        else if (forest[row, col] == 'W')
+                        {
+                            countOfWhite++;
+                            forest[row, col] = '-';
+                        }
+                        
+                    }
+                    else if(type == "Wild_Boar")
+                    {
+                        string direction = cmdArgs[3];
+                        while (IsInForest(row, col, forest))
+                        {
+                            if (direction == "down")
+                            {
+                                if (forest[row, col] == 'W')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+                                    
+                                }
+                                else if(forest[row, col] == 'S')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+                                    
+                                }
+                                else if (forest[row,col] == 'B')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+                                    
+                                }
+                               
+                                row += 2;
+                                
+                            }
+                            else if(direction == "up") 
+                            {
+                                if (forest[row, col] == 'W')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+                                  
+                                }
+                                else if (forest[row, col] == 'S')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+                                    
+                                }
+                                else if (forest[row, col] == 'B')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+                                   
+                                }
+                                row -= 2;
+                                
+                            }
+                            else if(direction == "right")
+                            {
+                                if (forest[row, col] == 'W')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+
+                                }
+                                else if (forest[row, col] == 'S')
+                                {
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+
+                                }
+                                else if (forest[row, col] == 'B')
+                                {
+                                    
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+
+                                }
+                                col += 2;
+                            }
+                            else if(direction == "left")
+                            {
+                                if (forest[row, col] == 'W')
+                                {
+                                    
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+
+                                }
+                                else if (forest[row, col] == 'S')
+                                {
+                                    
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+
+                                }
+                                else if (forest[row, col] == 'B')
+                                {
+                                    
+                                    forest[row, col] = '-';
+                                    wildBoarEaten++;
+
+                                }
+                                col -= 2;
+                            }
+                        }
+                    }
+                    command = Console.ReadLine();
+                    
+
+
+                }
+               
+
+            }
+            Console.WriteLine($"Peter manages to harvest {countOfBlack} black, {countOfSummer} summer, and {countOfWhite} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {wildBoarEaten} truffles.");
+            for(int row = 0; row < n; row++)
+            {
+                for(int col = 0; col < n; col++)
+                {
+                    Console.Write(forest[row, col] + " ");
+                }
+                Console.WriteLine();
+            } 
+        }
+        private static bool IsInForest(int row, int col, char[,] forest)
+        {
+            return row >= 0 && row < forest.GetLength(0) && 
+                col >= 0 && col < forest.GetLength(1);
+        }
+       
+    }
+}
